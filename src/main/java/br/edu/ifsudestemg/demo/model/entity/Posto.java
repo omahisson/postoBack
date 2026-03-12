7b98a90e32350e6760560b51140014c941a1a0a5
mahisson <mahisson@icloud.com>
2026-03-12 08:01:01 -0300

    parte 1

---
+++ src/main/java/br/edu/ifsudestemg/demo/model/entity/Posto.java
@@ -0,0 +1,15 @@
+package br.edu.ifsudestemg.demo.model.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Posto {
+
+}
