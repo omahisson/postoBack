995d1b98574f41fe3d1a2f33c1d9494195c5fc04
mahisson <mahisson@icloud.com>
2026-03-12 07:52:54 -0300

    start

---
+++ src/main/java/br/edu/ifsudestemg/demo/SeapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsudestemg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsudestemg/demo/SeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsudestemg.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
