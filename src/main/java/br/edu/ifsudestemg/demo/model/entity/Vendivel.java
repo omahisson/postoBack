4ab48ac8ea7f7d5e6f64b2689c516101929a2fea
srmarxx <marcosgabrieldiascardoso66@gmail.com>
2026-03-12 08:35:33 -0300

    Criando a classe abstrata vendível

---
+++ src/main/java/br/edu/ifsudestemg/demo/model/entity/Vendivel.java
@@ -0,0 +1,27 @@
+package br.edu.ifsudestemg.demo.model.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@MappedSuperclass
+public abstract class Vendivel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private String description;
+    private String unit;
+    private Boolean active;
+}
