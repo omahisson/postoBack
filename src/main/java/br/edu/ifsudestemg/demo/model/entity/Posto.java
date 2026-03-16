26a112840218544097bee5dc70fa08e67ba460ef
mahisson <mahisson@icloud.com>
2026-03-16 11:44:53 -0300

    posto

--- src/main/java/br/edu/ifsudestemg/demo/model/entity/Posto.java
+++ src/main/java/br/edu/ifsudestemg/demo/model/entity/Posto.java
@@ -1,12 +1,12 @@
 package br.edu.ifsudestemg.demo.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Entity
 @Data
@@ -16,6 +16,19 @@ import lombok.NoArgsConstructor;
 public class Posto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
 
+    private String nomeFantasia;
+    private String cnpj;
+    private String telefone;
+    private String email;
+    private String logradouro;
+    private int numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String cep;
+    @Column(name = "data_abertura")
+    private LocalDate dataAbertura;
+    private Boolean ativo;
 }
