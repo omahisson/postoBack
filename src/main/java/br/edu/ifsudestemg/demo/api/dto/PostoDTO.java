47948edf9295f3602ccad9f91c428449cb490f93
mahisson <mahisson@icloud.com>
2026-03-26 08:01:14 -0300

    dto posto

--- src/main/java/br/edu/ifsudestemg/demo/api/dto/PostoDTO.java
+++ src/main/java/br/edu/ifsudestemg/demo/api/dto/PostoDTO.java
@@ -1,13 +1,36 @@
 package br.edu.ifsudestemg.demo.api.dto;
 
+import br.edu.ifsudestemg.demo.model.entity.Posto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 
 public class PostoDTO {
+    private String telefone;
+    private String email;
+    private String logradouro;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String cep;
+
     private Long id;
+    private Long nome;
+    private LocalDate dataCadastro;
+    private Posto posto;
+    private Boolean ativo;
+
+    private String cnpj;
+    private String razaoSocial;
+    private String nomeFantasia;
+    private String inscricaoEstadual;
+
+    private LocalDate dataAbertura;
 }
