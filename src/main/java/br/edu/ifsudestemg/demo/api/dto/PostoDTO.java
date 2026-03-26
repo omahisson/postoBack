ab7f34a0705255357ea2b01cfa929a15a1ad76ac
mahisson <mahisson@icloud.com>
2026-03-26 07:53:14 -0300

    pastas

---
+++ src/main/java/br/edu/ifsudestemg/demo/api/dto/PostoDTO.java
@@ -0,0 +1,13 @@
+package br.edu.ifsudestemg.demo.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class PostoDTO {
+    private Long id;
+}
