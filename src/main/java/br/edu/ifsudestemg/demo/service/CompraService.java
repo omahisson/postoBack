f54c9616d45a192bc0c6cde6ce226cbe5115b422
Lacordia <pflacordia@gmail.com>
2026-04-14 09:24:20 -0300

    Completando service de compra, colocando salvamento, exclusão e validação

--- src/main/java/br/edu/ifsudestemg/demo/service/CompraService.java
+++ src/main/java/br/edu/ifsudestemg/demo/service/CompraService.java
@@ -1,10 +1,13 @@
 package br.edu.ifsudestemg.demo.service;
 
+import br.edu.ifsudestemg.demo.exception.RegraNegocioException;
 import br.edu.ifsudestemg.demo.model.entity.Compra;
-import br.edu.ifsudestemg.demo.model.entity.Promocao;
 import br.edu.ifsudestemg.demo.model.repository.CompraJpaRepository;
+import jakarta.transaction.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 public class CompraService {
@@ -20,5 +23,38 @@ public class CompraService {
 
     public Optional<Compra> getCompraById(Long id){
         return repository.findById(id);
+    }
+
+    @Transactional
+    public Compra salvar(Compra compra) {
+        validar(compra);
+        return repository.save(compra);
+    }
+
+    @Transactional
+    public void excluir(Compra compra) {
+        Objects.requireNonNull(compra.getId());
+        repository.delete(compra);
+    }
+
+    public void validar(Compra compra) {
+        if(compra.getDataCompra() == null || compra.getDataCompra().isBefore(LocalDate.now())){
+            throw new RegraNegocioException("Data inválida");
+        }
+        if(compra.getValorTotal() == null || compra.getValorTotal().doubleValue() < 0){
+            throw new RegraNegocioException("Valor total inválido");
+        }
+        if (compra.getNumeroNotaFiscal() == null || compra.getNumeroNotaFiscal().trim().isEmpty()) {
+            throw new RegraNegocioException("Nota Fiscal inválida.");
+        }
+        if (compra.getFormaPagamento() == null){
+            throw new RegraNegocioException("Forma de pagamento inválida");
+        }
+        if (compra.getPosto() == null){
+            throw new RegraNegocioException("Posto inválido");
+        }
+        if (compra.getStatus() == null){
+            throw new RegraNegocioException("Status inválido");
+        }
     }
 }
