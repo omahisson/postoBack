ee63dda33032377aa475573319bccd24ec965286
Lacordia <pflacordia@gmail.com>
2026-04-16 11:17:22 -0300

    Completando service dos registros de preço, colocando salvamento, exclusão e validação

--- src/main/java/br/edu/ifsudestemg/demo/service/RegistroPrecoProdutoService.java
+++ src/main/java/br/edu/ifsudestemg/demo/service/RegistroPrecoProdutoService.java
@@ -1,12 +1,16 @@
 package br.edu.ifsudestemg.demo.service;
 
+import br.edu.ifsudestemg.demo.exception.RegraNegocioException;
+import br.edu.ifsudestemg.demo.model.entity.RegistroPreco;
 import br.edu.ifsudestemg.demo.model.entity.RegistroPrecoProduto;
 import br.edu.ifsudestemg.demo.model.repository.RegistroPrecoProdutoJpaRepository;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
-public class RegistroPrecoProdutoService {
+public class RegistroPrecoProdutoService{
     private RegistroPrecoProdutoJpaRepository repository;
 
     public RegistroPrecoProdutoService(RegistroPrecoProdutoJpaRepository repository){
@@ -20,4 +24,35 @@ public class RegistroPrecoProdutoService {
     public Optional<RegistroPrecoProduto> getRegistroPrecoProdutoById(Long id){
         return repository.findById(id);
     }
+
+    @Transactional
+    public RegistroPrecoProduto salvar(RegistroPrecoProduto registroPrecoProduto) {
+        validar(registroPrecoProduto);
+        return repository.save(registroPrecoProduto);
+    }
+
+    @Transactional
+    public void excluir(RegistroPrecoProduto registroPrecoProduto) {
+        Objects.requireNonNull(registroPrecoProduto.getId());
+        repository.delete(registroPrecoProduto);
+    }
+
+    public void validar(RegistroPrecoProduto registroPrecoProduto) {
+        if(registroPrecoProduto.getValor() == null || registroPrecoProduto.getValor().doubleValue() < 0){
+            throw new RegraNegocioException("Valor inválido.");
+        }
+        if(registroPrecoProduto.getDataInicioVigencia() == null){
+            throw new RegraNegocioException("Data início inválida.");
+        }
+        if(registroPrecoProduto.getDataFimVigencia() == null){
+            throw new RegraNegocioException("Data fim inválida");
+        }
+        if(registroPrecoProduto.getAtivo() == null){
+            throw new RegraNegocioException("Status inválido.");
+        }
+        if (registroPrecoProduto.getProduto() == null) {
+            throw new RegraNegocioException("Produto inválido.");
+        }
+    }
+
 }
--- src/main/java/br/edu/ifsudestemg/demo/service/RegistroPrecoServicoService.java
+++ src/main/java/br/edu/ifsudestemg/demo/service/RegistroPrecoServicoService.java
@@ -1,12 +1,16 @@
 package br.edu.ifsudestemg.demo.service;
 
+import br.edu.ifsudestemg.demo.exception.RegraNegocioException;
+import br.edu.ifsudestemg.demo.model.entity.RegistroPrecoProduto;
 import br.edu.ifsudestemg.demo.model.entity.RegistroPrecoServico;
 import br.edu.ifsudestemg.demo.model.repository.RegistroPrecoServicoJpaRepository;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
-public class RegistroPrecoServicoService {
+public class RegistroPrecoServicoService{
     private RegistroPrecoServicoJpaRepository repository;
 
     public RegistroPrecoServicoService(RegistroPrecoServicoJpaRepository repository) {
@@ -20,4 +24,35 @@ public class RegistroPrecoServicoService {
     public Optional<RegistroPrecoServico> getRegistroPrecoServicoById(Long id){
         return repository.findById(id);
     }
+
+    @Transactional
+    public RegistroPrecoServico salvar(RegistroPrecoServico registroPrecoServico) {
+        validar(registroPrecoServico);
+        return repository.save(registroPrecoServico);
+    }
+
+    @Transactional
+    public void excluir(RegistroPrecoServico registroPrecoServico) {
+        Objects.requireNonNull(registroPrecoServico.getId());
+        repository.delete(registroPrecoServico);
+    }
+
+    public void validar(RegistroPrecoServico registroPrecoServico) {
+        if(registroPrecoServico.getValor() == null || registroPrecoServico.getValor().doubleValue() < 0){
+            throw new RegraNegocioException("Valor inválido.");
+        }
+        if(registroPrecoServico.getDataInicioVigencia() == null){
+            throw new RegraNegocioException("Data início inválida.");
+        }
+        if(registroPrecoServico.getDataFimVigencia() == null){
+            throw new RegraNegocioException("Data fim inválida");
+        }
+        if(registroPrecoServico.getAtivo() == null){
+            throw new RegraNegocioException("Status inválido.");
+        }
+        if (registroPrecoServico.getServico() == null) {
+            throw new RegraNegocioException("Produto inválido.");
+        }
+    }
+
 }
