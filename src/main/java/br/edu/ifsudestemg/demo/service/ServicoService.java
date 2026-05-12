0048e1434da26868552c96490674962d84dbb775
mahisson <mahisson@icloud.com>
2026-05-12 06:23:32 -0300

    correcao servicoservice

--- src/main/java/br/edu/ifsudestemg/demo/service/ServicoService.java
+++ src/main/java/br/edu/ifsudestemg/demo/service/ServicoService.java
@@ -4,10 +4,7 @@ import br.edu.ifsudestemg.demo.exception.RegraNegocioException;
 import br.edu.ifsudestemg.demo.model.entity.Servico;
 import br.edu.ifsudestemg.demo.model.repository.ServicoJpaRepository;
 import jakarta.transaction.Transactional;
-<<<<<<< HEAD
-=======
 import lombok.RequiredArgsConstructor;
->>>>>>> dc3577fbe146afbfe0fc8eaa31576fc55ad9b5af
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,12 +12,10 @@ import java.util.Objects;
 import java.util.Optional;
 
 @Service
-<<<<<<< HEAD
-=======
 @RequiredArgsConstructor
->>>>>>> dc3577fbe146afbfe0fc8eaa31576fc55ad9b5af
 public class ServicoService {
-    private ServicoJpaRepository repository;
+
+    private final ServicoJpaRepository repository;
 
     public List<Servico> getServicos(){
         return repository.findAll();
@@ -46,26 +41,33 @@ public class ServicoService {
         if(servico.getNome() == null || servico.getNome().trim().isEmpty()){
             throw new RegraNegocioException("Nome inválido");
         }
+
         if(servico.getPreco() == null || servico.getPreco().doubleValue() < 0){
             throw new RegraNegocioException("Preço inválido");
         }
+
         if(servico.getDescricao() == null || servico.getDescricao().trim().isEmpty()){
             throw new RegraNegocioException("Descrição inválida");
         }
+
         if(servico.getPosto() == null){
             throw new RegraNegocioException("Posto inválido");
         }
+
         if(servico.getAtivo() == null){
             throw new RegraNegocioException("Status inválido");
         }
+
         if(servico.getDuracaoEstimadaMinutos() == null || servico.getDuracaoEstimadaMinutos().trim().isEmpty()){
             throw new RegraNegocioException("Duração inválida");
         }
-        if (servico.getRequerAgendamento() == null){
+
+        if(servico.getRequerAgendamento() == null){
             throw new RegraNegocioException("Agendamento inválido");
         }
-        if (servico.getDescricaoTecnica() == null || servico.getDescricaoTecnica().trim().isEmpty()){
-            throw new RegraNegocioException("Descrição tecnica inválida");
+
+        if(servico.getDescricaoTecnica() == null || servico.getDescricaoTecnica().trim().isEmpty()){
+            throw new RegraNegocioException("Descrição técnica inválida");
         }
     }
 }
