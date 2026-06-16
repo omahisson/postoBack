b9df5cf464c4953e1eefe08219f0c97029a41d49
mahisson <mahisson@icloud.com>
2026-06-16 08:28:40 -0300

    parte 1 capitulo de segurança

--- src/main/java/br/edu/ifsudestemg/demo/security/JwtAuthFilter.java
+++ src/main/java/br/edu/ifsudestemg/demo/security/JwtAuthFilter.java
@@ -1,4 +1,52 @@
 package br.edu.ifsudestemg.demo.security;
 
-public class JwtAuthFilter {
+import br.edu.ifsudestemg.demo.service.UsuarioService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private JwtService jwtService;
+    private UsuarioService usuarioService;
+
+    public JwtAuthFilter( JwtService jwtService, UsuarioService usuarioService ) {
+        this.jwtService = jwtService;
+        this.usuarioService = usuarioService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest httpServletRequest,
+            HttpServletResponse httpServletResponse,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        String authorization = httpServletRequest.getHeader("Authorization");
+
+        if( authorization != null && authorization.startsWith("Bearer")){
+            String token = authorization.split(" ")[1];
+            boolean isValid = jwtService.tokenValido(token);
+
+            if(isValid){
+                String loginUsuario = jwtService.obterLoginUsuario(token);
+                UserDetails usuario = usuarioService.loadUserByUsername(loginUsuario);
+                UsernamePasswordAuthenticationToken user = new
+                        UsernamePasswordAuthenticationToken(usuario,null,
+                        usuario.getAuthorities());
+                user.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                SecurityContextHolder.getContext().setAuthentication(user);
+            }
+        }
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
 }
