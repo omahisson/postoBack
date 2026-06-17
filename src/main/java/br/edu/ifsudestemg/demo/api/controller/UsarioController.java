8ffee305c6f561cc430733d81694de6c8251de38
Lacordia <pflacordia@gmail.com>
2026-06-17 20:58:20 -0300

    usuario controller e tokendto feitas

---
+++ src/main/java/br/edu/ifsudestemg/demo/api/controller/UsarioController.java
@@ -0,0 +1,130 @@
+package br.edu.ifsudestemg.demo.api.controller;
+
+
+import br.edu.ifsudestemg.demo.api.dto.CredenciaisDTO;
+import br.edu.ifsudestemg.demo.api.dto.TokenDTO;
+import br.edu.ifsudestemg.demo.api.dto.UsuarioDTO;
+import br.edu.ifsudestemg.demo.exception.RegraNegocioException;
+import br.edu.ifsudestemg.demo.exception.SenhaInvalidaException;
+import br.edu.ifsudestemg.demo.model.entity.Usuario;
+import br.edu.ifsudestemg.demo.model.repository.UsuarioRepository;
+import br.edu.ifsudestemg.demo.security.JwtService;
+import br.edu.ifsudestemg.demo.service.UsuarioService;
+import io.jsonwebtoken.Jwt;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("api/v1/usuarios")
+@RequiredArgsConstructor
+@CrossOrigin
+public class UsarioController {
+
+    private final UsuarioService usuarioService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    private final UsuarioService service;
+
+    @GetMapping()
+    public ResponseEntity get() {
+        List<Usuario> usuarios = service.getUsuarios();
+        return ResponseEntity.ok(usuarios.stream().map(UsuarioDTO::create).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity get(@PathVariable("id") Long id) {
+        Optional<Usuario> usuario = service.getUsuarioById(id);
+        if (!usuario.isPresent()) {
+            return new ResponseEntity("Usuário não encontrado", HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(usuario.map(UsuarioDTO::create));
+    }
+
+    @PostMapping()
+    public ResponseEntity post(@RequestBody UsuarioDTO dto) {
+        try {
+            if (dto.getSenha() == null || dto.getSenha().trim().equals("") ||
+                    dto.getSenhaRepeticao() == null || dto.getSenhaRepeticao().trim().equals("")) {
+                return ResponseEntity.badRequest().body("Senha inválida");
+            }
+            if (!dto.getSenha().equals(dto.getSenhaRepeticao())) {
+                return ResponseEntity.badRequest().body("Senhas não conferem");
+            }
+            Usuario usuario = converter(dto);
+            String senhaCriptografada = passwordEncoder.encode(dto.getSenha());
+            usuario.setSenha(senhaCriptografada);
+            usuario = service.salvar(usuario);
+            return new ResponseEntity(usuario, HttpStatus.CREATED);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/auth")
+    public TokenDTO autenticar(@RequestBody CredenciaisDTO credenciais){
+        try{
+            Usuario usuario = Usuario.builder()
+                    .login(credenciais.getLogin())
+                    .senha(credenciais.getSenha()).build();
+            UserDetails usuarioAutenticado = usuarioService.autenticar(usuario);
+            String token = jwtService.gerarToken(usuario);
+            return new TokenDTO(usuario.getLogin(), token);
+        } catch (UsernameNotFoundException | SenhaInvalidaException e ){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        }
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity atualizar(@PathVariable("id") Long id, @RequestBody UsuarioDTO dto) {
+        if (!service.getUsuarioById(id).isPresent()) {
+            return new ResponseEntity("Usuário não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            if (dto.getSenha() == null || dto.getSenha().trim().equals("") ||
+                    dto.getSenhaRepeticao() == null || dto.getSenhaRepeticao().trim().equals("")) {
+                return ResponseEntity.badRequest().body("Senha inválida");
+            }
+            if (!dto.getSenha().equals(dto.getSenhaRepeticao())) {
+                return ResponseEntity.badRequest().body("Senhas não conferem");
+            }
+            Usuario usuario = converter(dto);
+            usuario.setId(id);
+            service.salvar(usuario);
+            return ResponseEntity.ok(usuario);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity delete(@PathVariable("id") Long id) {
+        Optional<Usuario> usuario = service.getUsuarioById(id);
+        if (!usuario.isPresent()) {
+            return new ResponseEntity("Usuário não encontrado", HttpStatus.NOT_FOUND);
+        }
+        try {
+            service.excluir(usuario.get());
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } catch (RegraNegocioException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    public Usuario converter(UsuarioDTO dto) {
+        ModelMapper modelMapper = new ModelMapper();
+        Usuario usuario = modelMapper.map(dto, Usuario.class);
+        return usuario;
+    }
+}
